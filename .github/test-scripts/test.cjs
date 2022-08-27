4083c814e8f328b916168d5f3a9f2ed234101255
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-08-27 11:51:47 +0200

    feat: convert to hybrid package

---
+++ .github/test-scripts/test.cjs
@@ -0,0 +1,4 @@
+const ipRegex = require("@esm2cjs/ip-regex").default;
+const assert = require("assert");
+
+assert(typeof ipRegex === "function");
---
+++ .github/test-scripts/test.mjs
@@ -0,0 +1,4 @@
+import ipRegex from "@esm2cjs/ip-regex";
+import assert from "assert";
+
+assert(typeof ipRegex === "function");
